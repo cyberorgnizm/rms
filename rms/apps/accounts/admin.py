f28e9bd2ed888f19ad0f27fa3c3a8c3c707c7452
Adole Samuel <adoleichasamuel@gmail.com>
2021-07-07 17:12:51 +0100

    feat: modified accounts app models

--- rms/apps/accounts/admin.py
+++ rms/apps/accounts/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AbstractUserAdmin
 from django.contrib.auth import get_user_model
+from . import models
+
 
 @admin.register(get_user_model())
 class UserAdmin(AbstractUserAdmin):
+    pass
+
+
+@admin.register(models.Student)
+class StudentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Worker)
+class WorkerAdmin(admin.ModelAdmin):
     pass
--- rms/apps/accounts/models.py
+++ rms/apps/accounts/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    phone = models.CharField(max_length=255, null=True, blank=True)
     bio = models.TextField(verbose_name="About Me", blank=True, null=True)
-    matric = models.CharField(max_length=255)
-    is_student = models.BooleanField(default=False)
+    address = models.TextField()
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}" 
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    matric_no = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.user} ({self.matric_no})"
+
+class Worker(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_available = models.BooleanField(default=True)
+    role = models.CharField(max_length=255, choices=(('Chef', 'Chef'), ('Delivery', 'Delivery')))
+
+    def __str__(self):
+        return f"{self.user} ({self.role})"
