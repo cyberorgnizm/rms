6dfb66648921b9692eae3860d34bd115c520e60e
Adole Samuel <adoleichasamuel@gmail.com>
2021-07-30 01:24:27 +0100

    🚀 configure project for django deployment

--- rms/settings.py
+++ rms/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from os import getenv
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django shopping cart settings
 CART_SESSION_ID = 'cart'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
