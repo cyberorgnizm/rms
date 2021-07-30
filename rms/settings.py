4b8be82916c88126c55cea8504a8ef1cd64ce45b
Adole Samuel <adoleichasamuel@gmail.com>
2021-07-30 01:34:19 +0100

    fix: static root config

--- rms/settings.py
+++ rms/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "rms/static",
 ]
