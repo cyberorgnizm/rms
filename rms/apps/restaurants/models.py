5ae96fcc809657c0d43aeca4580a18b299d8766d
Adole Samuel <adoleichasamuel@gmail.com>
2021-07-30 01:06:39 +0100

    fix: alter migration for  price of menu items

--- rms/apps/restaurants/models.py
+++ rms/apps/restaurants/models.py
@@ -57,7 +57,7 @@ class Menu(models.Model):
     description = models.TextField()
     slug = models.SlugField(blank=True, unique=True)
     menu_type = models.CharField(max_length=255, choices=MENU_TYPES)
-    price = models.DecimalField(decimal_places=2, max_digits=5)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
 
