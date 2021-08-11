cabc227c078534e43d708a36287bdc255cb810c8
Adole Samuel <adoleichasamuel@gmail.com>
2021-08-12 00:52:48 +0100

    feat: add rating preview for menus

--- rms/views.py
+++ rms/views.py
@@ -1,15 +1,21 @@
 from django.views import generic
 from django.db.models import F, Avg
-from .apps.restaurants.models import Cafeteria
+from .apps.restaurants.models import Cafeteria, Menu
 
 class HomeView(generic.TemplateView):
     template_name = "pages/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # most liked cafeterias
         cafeteria_rating_filter = Cafeteria.objects.all().filter(reviews__rating__gte=3)
-        distinct_cafeteria = cafeteria_rating_filter.distinct()
-        context["top_cafeterias"] = distinct_cafeteria.order_by('name').annotate(avg_reviews=Avg(F('reviews__rating')))
+        distinct_cafeteria = cafeteria_rating_filter.distinct()[:3]
+        context["top_cafeterias"] = distinct_cafeteria.annotate(avg_reviews=Avg(F('reviews__rating')))
+
+        # most liked menus
+        menu_rating_filter = Menu.objects.all().filter(reviews__rating__gte=3)
+        distinct_menu = menu_rating_filter.distinct()[:3]
+        context["top_menus"] = distinct_menu.annotate(avg_reviews=Avg(F('reviews__rating')))
         return context
 
 class AboutView(generic.TemplateView):
